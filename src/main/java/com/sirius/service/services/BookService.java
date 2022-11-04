d837d072fb4c2fbb67a98fd52605554bf4b04d93
SiriusEdwin <jfk7500@gmail.com>
2022-11-03 21:25:52 -0600

    Initial commit

---
+++ src/main/java/com/sirius/service/services/BookService.java
@@ -0,0 +1,40 @@
+package com.sirius.service.services;
+
+import com.sirius.service.entities.Book;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@ApplicationScoped
+public class BookService {
+    @Inject
+    EntityManager em;
+    public List getAll() {
+        List<Book> books = em.createNamedQuery("Player.findAll", Book.class)
+                .getResultList();
+        return books != null ? books : new ArrayList<>();
+    }
+    public Book findById(Long id) {
+        return em.find(Book.class, id);
+    }
+    @Transactional
+    public void update(Book book) {
+        em.merge(book);
+    }
+    @Transactional
+    public void create(Book book) {
+        em.persist(book);
+    }
+    @Transactional
+    public void delete(Book book) {
+        if (!em.contains(book)) {
+            book = em.merge(book);
+        }
+        em.remove(book);
+    }
+}
